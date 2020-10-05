c901aca825e710db8de2c396b2a19786ce48c025
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-10-05 13:28:49 +0330

    Bootcamp create functionality

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ connectDB()
 
 const app = express()
 
+// Body Parser
+app.use(express.json())
+
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
@@ -25,7 +28,7 @@ app.use("/api/v1/bootcamps", bootcampsRoutes)
 
 const PORT = process.env.PORT || 5000
 
-const sever = app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
