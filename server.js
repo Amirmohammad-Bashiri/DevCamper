85f7988089d14f65d1b10328320dc231ebfe23f9
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-10-05 12:01:54 +0330

    Database setup

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
+const connectDB = require("./config/db")
 
 // Route files
 const bootcampsRoutes = require("./routes/bootcamps")
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" })
+
+// Database connection
+connectDB()
 
 const app = express()
 
@@ -20,9 +24,16 @@ app.use("/api/v1/bootcamps", bootcampsRoutes)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const sever = app.listen(
   PORT,
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 )
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
