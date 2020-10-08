f579293c6648d083639d03218a4c72cc3d5cd6b5
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-10-08 13:05:34 +0330

    Custom ErrorResponse Class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require("../models/Bootcamp")
+const ErrorResponse = require("../utils/errorResponse")
 
 // @desc      Get all bootcamps
 // @rotue     GET /api/v1/bootcamps
@@ -25,13 +26,23 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id)
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false })
+      return next(
+        new ErrorResponse(
+          `Bootcamp could not be found with the id of ${req.params.id}`,
+          404
+        )
+      )
     }
 
     res.status(200).json({ success: true, data: bootcamp })
   } catch (err) {
     // res.status(400).json({ success: false })
-    next(err)
+    next(
+      new ErrorResponse(
+        `Bootcamp could not be found with the id of ${req.params.id}`,
+        404
+      )
+    )
   }
 }
 
