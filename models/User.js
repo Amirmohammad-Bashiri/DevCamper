a0b894e2cc60aaabbfd5b144ca80413d3fcec168
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-11-01 12:08:58 +0330

    User password encryption

--- models/User.js
+++ models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please add a name"]
+    required: [true, "Please add a name"],
   },
   email: {
     type: String,
@@ -16,20 +17,28 @@ const UserSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ["user", "publisher"],
-    default: "user"
+    default: "user",
   },
   password: {
     type: String,
     required: [true, "Please add a password"],
     minlength: 6,
-    select: false
+    select: false,
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+})
+
+// Encrypt password
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+
+  next()
 })
 
 module.exports = mongoose.model("User", UserSchema)
