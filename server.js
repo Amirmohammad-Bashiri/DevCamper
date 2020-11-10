d617344114803960c16fcb9a6b25430fe0a2b741
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-11-10 17:11:44 +0330

    Rate limiting, hpp and CORS

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const colors = require("colors")
 const mongoSanitize = require("express-mongo-sanitize")
 const helmet = require("helmet")
 const xss = require("xss-clean")
+const rateLimit = require("express-rate-limit")
+const hpp = require("hpp")
+const cors = require("cors")
 const fileupload = require("express-fileupload")
 const cookieParser = require("cookie-parser")
 const errorHandler = require("./middleware/error")
@@ -48,6 +51,20 @@ app.use(helmet())
 
 // Prevent XSS attacks
 app.use(xss())
+
+// Enable CORS
+app.use(cors())
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+})
+
+app.use(limiter)
+
+// Prevent http param polution
+app.use(hpp())
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")))
