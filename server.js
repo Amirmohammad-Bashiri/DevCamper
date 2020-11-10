17e1a342fa403b5a201209e33298bdae76f104cf
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-11-10 17:02:57 +0330

    XSS protection headers

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv")
 const morgan = require("morgan")
 const colors = require("colors")
 const mongoSanitize = require("express-mongo-sanitize")
+const helmet = require("helmet")
+const xss = require("xss-clean")
 const fileupload = require("express-fileupload")
 const cookieParser = require("cookie-parser")
 const errorHandler = require("./middleware/error")
@@ -40,6 +42,12 @@ app.use(fileupload())
 
 // Sanitize data
 app.use(mongoSanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xss())
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")))
